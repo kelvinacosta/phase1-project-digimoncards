afd6a61b8cc4c55a312f79e771405d573f969157
Kelvin Acosta <acosta.kelvin02@gmail.com>
2023-05-14 17:38:41 -0400

    appends tags to digimon list

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+//Create a variable to get the class of a html list
+const digimonList = document.querySelector('.digimonList')
+
 //Fetching digimon api from data
 fetch('https://digimon-api.vercel.app/api/digimon')
 .then(res => res.json())
@@ -11,11 +14,16 @@ fetch('https://digimon-api.vercel.app/api/digimon')
     //Create tags name,img and level to append to the list digimon
     const digimonImage = document.createElement('img')
     digimonImage.src = digimon.img 
-    console.log(digimonImage)
+    
     
     const digimonName = document.createElement('h2')
     digimonName.textContent = digimon.name
     //console.log(digimonName)
+
+    //Appending tags to list of digimon <li>
+    listDigimon.appendChild(digimonImage)
+    listDigimon.appendChild(digimonName)
+    console.log(listDigimon)
 
 
 
