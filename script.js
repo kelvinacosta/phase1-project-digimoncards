2bf38861c600280c0625609157eb154f3673779f
Kelvin Acosta <acosta.kelvin02@gmail.com>
2023-05-14 17:33:04 -0400

    create tags to append to the list digimon

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 //Fetching digimon api from data
 fetch('https://digimon-api.vercel.app/api/digimon')
 .then(res => res.json())
-.then(data => console.log(data))
+.then(data => data.forEach(digimon => {
+    
+    //create a list of digimon to store each item
+    const listDigimon = document.createElement('li')
+    listDigimon.classList.add('.digimon')
+    //console.log(listDigimon)
+
+    //Create tags name,img and level to append to the list digimon
+    const digimonImage = document.createElement('img')
+    digimonImage.src = digimon.img 
+    console.log(digimonImage)
+    
+    const digimonName = document.createElement('h2')
+    digimonName.textContent = digimon.name
+    //console.log(digimonName)
+
+
+
+
+}))
