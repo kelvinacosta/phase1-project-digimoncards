dcda3f4e92c9eaf2a0325610763ebd5d049eaa95
Kelvin Acosta <acosta.kelvin02@gmail.com>
2023-05-14 17:56:07 -0400

    creates a button like and 2 variables to store an empty heart

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+//Create 2 variables to store the like button functionality whit an emoji context of a heart
+const empyHeart = '🤍';
+const fullHeart= '❤️';
+
 //Create a variable to get the class of a html list
 const digimonList = document.querySelector('.digimonList')
 
@@ -17,17 +21,23 @@ const fetchData = ()=>{
         //Create tags name,img and level to append to the list digimon
         const digimonImage = document.createElement('img')
         digimonImage.src = digimon.img 
+        digimonImage.alt = digimon.img 
         
         
         const digimonName = document.createElement('h2')
         digimonName.textContent = digimon.name
         //console.log(digimonName)
+
+        //create a button and then add a click event to button
+        const likeButton = document.createElement('button')
+        likeButton.textContent = empyHeart
     
         //Appending tags to list of digimon <li>
         listDigimon.appendChild(digimonImage)
         listDigimon.appendChild(digimonName)
+        listDigimon.appendChild(likeButton)
         //console.log(listDigimon)
-    
+        
         digimonList.appendChild(listDigimon)
     
     
@@ -35,6 +45,10 @@ const fetchData = ()=>{
     
     
     }))
+    //adds a catch error 
+    .catch(error=>{
+        console.log(error)
+    })
 } 
 
 //calling function fetchdata
