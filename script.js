cd193156dc37c72d498d10bf693055539b9ee399
Kelvin Acosta <acosta.kelvin02@gmail.com>
2023-05-14 17:46:56 -0400

    creates a variable to store a function fetch and then call it

--- script.js
+++ script.js
@@ -1,34 +1,41 @@
 //Create a variable to get the class of a html list
 const digimonList = document.querySelector('.digimonList')
 
-//Fetching digimon api from data
-fetch('https://digimon-api.vercel.app/api/digimon')
-.then(res => res.json())
-.then(data => data.forEach(digimon => {
+//create a variable to store a fetch and then call it
+const fetchData()=>{
+
+    //Fetching digimon api from data
+    fetch('https://digimon-api.vercel.app/api/digimon')
+    .then(res => res.json())
+    .then(data => data.forEach(digimon => {
+        
+        //create a list of digimon to store each item
+        const listDigimon = document.createElement('li')
+        listDigimon.classList.add('.digimon')
+        //console.log(listDigimon)
     
-    //create a list of digimon to store each item
-    const listDigimon = document.createElement('li')
-    listDigimon.classList.add('.digimon')
-    //console.log(listDigimon)
-
-    //Create tags name,img and level to append to the list digimon
-    const digimonImage = document.createElement('img')
-    digimonImage.src = digimon.img 
+        //Create tags name,img and level to append to the list digimon
+        const digimonImage = document.createElement('img')
+        digimonImage.src = digimon.img 
+        
+        
+        const digimonName = document.createElement('h2')
+        digimonName.textContent = digimon.name
+        //console.log(digimonName)
+    
+        //Appending tags to list of digimon <li>
+        listDigimon.appendChild(digimonImage)
+        listDigimon.appendChild(digimonName)
+        //console.log(listDigimon)
+    
+        digimonList.appendChild(listDigimon)
     
     
-    const digimonName = document.createElement('h2')
-    digimonName.textContent = digimon.name
-    //console.log(digimonName)
+    
+    
+    
+    }))
+} 
 
-    //Appending tags to list of digimon <li>
-    listDigimon.appendChild(digimonImage)
-    listDigimon.appendChild(digimonName)
-    //console.log(listDigimon)
-
-    digimonList.appendChild(listDigimon)
-
-
-
-
-
-}))
+//calling function fetchdata
+fetchData()
